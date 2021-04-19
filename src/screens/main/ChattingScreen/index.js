ff70d4ff6c822179e17cf64f4978d1ea8d171a28
MobileHorse <58142664+MobileHorse@users.noreply.github.com>
2021-04-19 12:24:22 +0300

    Initial

--- src/screens/main/ChattingScreen/index.js
+++ src/screens/main/ChattingScreen/index.js
@@ -11,11 +11,26 @@ import {
 } from 'react-native';
 import {connect} from 'react-redux';
 import {styles} from './style';
-
+import {AppStyles} from "../../../constants/appStyles";
+import {default as AntDesignIcon} from "react-native-vector-icons/AntDesign";
+import {default as IonIcon} from "react-native-vector-icons/Ionicons";
 
 const ChattingScreen = props => {
     return (
-        <View />
+        <View style={AppStyles.container}>
+            <StatusBar backgroundColor="#ffffff" barStyle="dark-content"/>
+            <View style={[AppStyles.header, {paddingHorizontal: 20}]}>
+                <TouchableOpacity onPress={() => props.navigation.goBack()}>
+                    <AntDesignIcon name='arrowleft' color='#212B4B' size={20}/>
+                </TouchableOpacity>
+                <Image source={{uri: "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg"}} style={styles.avatar}/>
+                <Text style={styles.name}>Red Glove</Text>
+                <View style={{flex: 1}} />
+                <TouchableOpacity>
+                    <IonIcon name='ellipsis-horizontal-outline' color='#8F9BB3' size={20}/>
+                </TouchableOpacity>
+            </View>
+        </View>
     );
 };
 
--- src/screens/main/ChattingScreen/style.js
+++ src/screens/main/ChattingScreen/style.js
@@ -1,3 +1,14 @@
 import {StyleSheet} from 'react-native';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+    avatar: {
+        width: 40,
+        height: 40,
+        borderRadius: 40 / 2,
+        marginHorizontal: 20
+    },
+    name: {
+        fontSize: 15,
+        color: '#222B45'
+    }
+});
